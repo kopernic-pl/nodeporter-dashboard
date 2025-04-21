fb0beb1360dd6e43714035c19330967d791f8a83
Mateusz Wronski <mateusz.wronski@smartbear.com>
2025-04-21 18:11:49 +0200

    Add persistent retro-styled sticky footer with project name and GitHub link to all pages

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,24 @@
+import React from 'react';
 import '../styles/globals.css';
+import '@fontsource/press-start-2p';
+import '@fontsource/vt323';
+
+function Footer() {
+  return (
+    <footer className="retro-footer">
+      <span>Windsurf Project &nbsp;|&nbsp;</span>
+      <a href="https://github.com/" target="_blank" rel="noopener noreferrer">
+        GitHub
+      </a>
+    </footer>
+  );
+}
 
 export default function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Component {...pageProps} />
+      <Footer />
+    </>
+  );
 }
