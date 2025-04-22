2779aadce01da8721124fd85f01895cd9931841d
Mateusz Wronski <mateusz.wronski@smartbear.com>
2025-04-22 20:25:16 +0200

    chore: update manifest, webmanifest, favicons, and head links for Windsurf branding and multi-device support

--- pages/_document.js
+++ pages/_document.js
@@ -2,6 +2,8 @@ import React from 'react';
 import Document from 'next/document';
 import { ServerStyleSheet } from 'styled-components';
 
+import Head from 'next/head';
+
 export default class MyDocument extends Document {
   static async getInitialProps(ctx) {
     const sheet = new ServerStyleSheet();
@@ -16,6 +18,12 @@ export default class MyDocument extends Document {
         ...initialProps,
         styles: (
           <>
+            <Head>
+              <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+              <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+              <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+              <link rel="manifest" href="/site.webmanifest" />
+            </Head>
             {initialProps.styles}
             {sheet.getStyleElement()}
           </>
