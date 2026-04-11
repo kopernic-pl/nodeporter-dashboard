a81b7cfe59bd34743954b53bf5c98f32ea790562
Mateusz Wronski <mateusz.wronski@smartbear.com>
2026-04-11 17:32:55 +0200

    fix: eslint

--- __tests__/components/ExternalLinkIcon.test.js
+++ __tests__/components/ExternalLinkIcon.test.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { render, screen } from '@testing-library/react';
+import { render } from '@testing-library/react';
 import ExternalLinkIcon from '../../components/ExternalLinkIcon';
 
 // Suppress styled-jsx warnings for tests
