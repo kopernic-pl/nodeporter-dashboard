3c3dff9bdd2924ec95e06743bdc1a03eb68f2849
Mateusz Wronski <mateusz.wronski@smartbear.com>
2026-04-11 16:25:23 +0200

    feat: test coverage calculations

--- jest.config.js
+++ jest.config.js
@@ -8,4 +8,30 @@ module.exports = {
     '^.+\\.[jt]sx?$': ['babel-jest', { configFile: './babel.config.jest.js' }],
   },
   transformIgnorePatterns: ['node_modules/(?!(.*@faker-js/faker/.*|@faker-js/faker))'],
+  collectCoverage: true,
+  coverageDirectory: 'coverage',
+  coverageReporters: ['text', 'lcov', 'html', 'json-summary'],
+  coverageThreshold: {
+    global: {
+      branches: 70,
+      functions: 70,
+      lines: 70,
+      statements: 70
+    }
+  },
+  collectCoverageFrom: [
+    'components/**/*.{js,jsx}',
+    'hooks/**/*.{js,jsx}',
+    'pages/**/*.{js,jsx}',
+    'utils/**/*.{js,jsx}',
+    '!**/*.test.{js,jsx}',
+    '!**/*.spec.{js,jsx}',
+    '!**/node_modules/**',
+    '!**/coverage/**',
+    '!jest.config.js',
+    '!jest.setup.js',
+    '!babel.config.jest.js',
+    '!next.config.mjs',
+    '!eslint.config.mjs'
+  ]
 };
